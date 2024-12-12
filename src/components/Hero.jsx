7d291f5fe799e5e33a1864529d3a01cfce733f8e
hprasad <hrishikeshprasadc@gmail.com>
2024-12-12 20:43:11 +0530

    highlights section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,10 @@ const Hero = () => {
     }
 
     useEffect(()=>{
-        window.addEventListener('resize',handleVideoSrcSet)
+        window.addEventListener('resize',handleVideoSrcSet) // add an eventlistener when screen is resized it handles the function
 
         return ()=>{
-            window.removeEventListener('resize',handleVideoSrcSet)
+            window.removeEventListener('resize',handleVideoSrcSet) // in react it is really important to remove or clean the eventlistener after usage
         }
     },[])
 
@@ -28,17 +28,26 @@ const Hero = () => {
         gsap.to('#hero',{
             opacity:1,
             delay:2
+        }),
+        gsap.to('#cta',{
+            opacity:1,
+            delay:2,
+            y:-50
         })
     },[])
   return (
     <section className='w-full nav-height bg-black relative'>
         <div className='h-5/6 w-full flex-center flex-col'>
-            <p id='hero' className='hero-title'>iphone 16 pro</p>
+            <p id='hero' className='hero-title'>Iphone 16 pro</p>
             <div className='md:w-10/12 w-9/12'>
                 <video className='pointer-events-none' autoPlay  muted playsInline={true} key={videoSrc} >
                     <source src={videoSrc} type='video/mp4'/>
                 </video>
             </div>
+        </div>
+        <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
+            <a href="#highlights" className='btn'>Buy</a>
+            <p className='font-normal text-xl'>From $199/month or $999</p>
         </div>
     </section>
   )
